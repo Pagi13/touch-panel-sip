1fa1a2286221857c6cdec28e70b0b1001ab306a8
Denis Pagáč <denis.pagac@avesoft.cz>
2024-01-15 17:58:31 +0100

    a

--- index.js
+++ index.js
@@ -18,35 +18,37 @@ var server = app.listen(3000, () => {
 app.get("/call/:extension", (req, res) => {
   console.log(req.params.extension);
   exec(
-    "twinkle --call " + req.params.extension + " --immediate --config /home/kiosek/.twinkle/8999.cfg --user=8999 --calleenumber=9000 --nogui",
+    "twinkle --call " +
+      req.params.extension +
+      " --immediate --config /home/kiosek/.twinkle/8999.cfg --user=8999 --calleenumber=9000 --nogui",
     (error, stdout, stderr) => {
       if (error) {
-        console.log(`error: $ {error.message}`);
+        res.send('{"status": "error"}');
         return;
       }
       if (stderr) {
-        console.log(`stderr: ${stderr}`);
+        res.send('{"status": "good"}');
         return;
       }
-      console.log(`stdout: ${stdout}`);
+      res.send('{"status": "good"}');
     }
   );
 });
 
 app.get("/bye", (req, res) => {
-  console.log('bye');
-  exec(
-    "twinkle --cmd bye",
-    (error, stdout, stderr) => {
-      if (error) {
-        // console.log(`error: $ {error.message}`);
-        return;
-      }
-      if (stderr) {
-        // console.log(`stderr: ${stderr}`);
-        return;
-      }
-      // console.log(`stdout: ${stdout}`);
+  console.log("bye");
+  exec("twinkle --cmd bye", (error, stdout, stderr) => {
+    if (error) {
+      res.send('{"status": "error"}');
+      // console.log(`error: $ {error.message}`);
+      return;
     }
-  );
+    if (stderr) {
+      // console.log(`stderr: ${stderr}`);
+      res.send('{"status": "good"}');
+      return;
+    }
+    res.send('{"status": "error"}');
+    // console.log(`stdout: ${stdout}`);
+  });
 });
